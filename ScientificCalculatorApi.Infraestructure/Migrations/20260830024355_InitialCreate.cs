3ef89471ca574970fba91fdcd8020caf2ddbd3be
zdudusz <eduardogamese8@gmail.com>
2026-08-30 10:30:10 -0300

    Classe UserRepository com metodo de cadastro no banco de dados

---
+++ ScientificCalculatorApi.Infraestructure/Migrations/20260830024355_InitialCreate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ScientificCalculatorApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CalculationHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    Operation = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Parameters = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Result = table.Column<decimal>(type: "numeric(18,8)", precision: 18, scale: 8, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("CalculationHistory_pkey", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Email = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: false),
+                    PasswordHash = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Users_pkey", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "Users_Email_key",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CalculationHistory");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ ScientificCalculatorApi.Infraestructure/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using ScientificCalcAPI.Core.Interface.Repositories;
+using ScientificCalcAPI.Core.Entities;
+
+
+namespace ScientificCalculatorApi.Infrastructure.Repositories
+{
+    public class UserRepository(ScientificCalculatorContext dbContext) : IUserRepository
+    {
+        private readonly ScientificCalculatorContext _dbContext = dbContext;
+
+        // Metodo para cadastrar um novo usuário no banco de dados
+        public async Task<int> CadastrarAsync(User user) {
+           var entidade = await _dbContext.Users.AddAsync(user);
+            
+            await _dbContext.SaveChangesAsync();
+
+            return entidade.Entity.Id;
+        }
+    }
+}
