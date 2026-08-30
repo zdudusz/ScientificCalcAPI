03214dfa8e4b33f97fdc9e08d0920fefd546c69b
zdudusz <eduardogamese8@gmail.com>
2026-08-30 18:24:31 -0300

    interface de cadastrarUserApplication adicionada

---
+++ ScientificCalc.Application/Applications/ApplicationModule.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ScientificCalcAPI.Core.Interface.Applications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScientificCalcApi.Application.Applications
+{
+    public static class ApplicationModule
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+
+            services.AddScoped<ICadastrarUserApplication, CadastrarUserApplication>();
+            return services;
+        }
+    }
+}
---
+++ ScientificCalc.Application/Applications/CadastrarUserApplication.cs
@@ -0,0 +1,27 @@
+﻿using ScientificCalcAPI.Core.Entities;
+using ScientificCalcAPI.Core.Interface.Applications;
+using ScientificCalcAPI.Core.Interface.Repositories;
+using ScientificCalcAPI.Core.Models.InputModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ScientificCalcApi.Application.Applications
+{
+    public class CadastrarUserApplication(IUserRepository userRepository): ICadastrarUserApplication
+    {// Implementação da interface ICadastrarUserApplication para cadastrar um usuário no banco de dados
+        private readonly IUserRepository _userRepository = userRepository;
+
+        public async Task<int> CadastrarAsync(UserInputModel userInputModel) 
+        {
+            var user = new User(
+                userInputModel.Name, 
+                userInputModel.Email, 
+                userInputModel.Password);
+            
+            return await _userRepository.CadastrarAsync(user);
+        }
+     
+    }
+}
