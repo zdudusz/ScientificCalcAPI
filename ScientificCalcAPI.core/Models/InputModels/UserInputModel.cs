ffa1ce410a785a70ee741c1c92bf274c0d56126e
zdudusz <eduardogamese8@gmail.com>
2026-08-30 11:08:29 -0300

    feat: adiciona UserInputModel

---
+++ ScientificCalcAPI.core/Models/InputModels/UserInputModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScientificCalcAPI.Core.Models.InputModels
+{
+    public class UserInputModel(string name, string email, string passwordHash, DateTime createdAt)
+    {
+
+        public string Name { get;} = name;
+
+        public string Email { get;} = email!;
+
+        public string PasswordHash { get; } = passwordHash!;
+
+        public DateTime CreatedAt { get; } = createdAt;
+
+    }
+}
+
