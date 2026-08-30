30f94e046cfc161d8b6bc84ee31cb233266ccac8
zdudusz <eduardogamese8@gmail.com>
2026-08-29 22:52:12 -0300

    Adicionando classes de infraestructure module e DB context do projeto

---
+++ ScientificCalculatorApi.Infraestructure/InfrastructureModule.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScientificCalculatorApi.Infraestructure
+{
+    public static class InfrastructureModule
+    {
+        public static IServiceCollection AddInfraestructure(this IServiceCollection services, IConfiguration configuration) { 
+        services.AddDbContext<ScientificCalculatorContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("Server=localhost;Port=5490;Database=CALCDB;Username=calculator;Password=admin")));
+
+            return services;
+        }
+    }
+}
---
+++ ScientificCalculatorApi.Infraestructure/ScientificCalculatorContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScientificCalcAPI.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ScientificCalculatorApi.Infraestructure
+{
+    public class ScientificCalculatorContext(DbContextOptions options) : DbContext(options)
+    {
+        public virtual DbSet<CalculationHistory> CalculationHistories { get; set; }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected  override void OnModelCreating(ModelBuilder modelBuilder) {
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
