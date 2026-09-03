e43819f1b9b9c8ef10506eea6e6023b120e84fbd
zdudusz <eduardogamese8@gmail.com>
2026-09-03 20:17:14 -0300

    feat: adiciona hash de criptografia para senha

--- ScientificCalc.Application/Applications/CadastrarUserApplication.cs
+++ ScientificCalc.Application/Applications/CadastrarUserApplication.cs
@@ -2,9 +2,7 @@
 using ScientificCalcAPI.Core.Interface.Applications;
 using ScientificCalcAPI.Core.Interface.Repositories;
 using ScientificCalcAPI.Core.Models.InputModels;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using BCrypt.Net;
 
 
 namespace ScientificCalcApi.Application.Applications
@@ -13,12 +11,15 @@ namespace ScientificCalcApi.Application.Applications
     {// Implementação da interface ICadastrarUserApplication para cadastrar um usuário no banco de dados
         private readonly IUserRepository _userRepository = userRepository;
 
+        
         public async Task<int> CadastrarAsync(UserInputModel userInputModel) 
         {
+            var passwordHash = BCrypt.Net.BCrypt.HashPassword(userInputModel.Password); // Hashing da senha do usuário usando BCrypt
+
             var user = new User(
                 userInputModel.Name, 
                 userInputModel.Email, 
-                userInputModel.Password);
+                passwordHash);
             
             return await _userRepository.CadastrarAsync(user);
         }
