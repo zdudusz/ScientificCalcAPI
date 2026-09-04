b4d3ff1faa35e6e1d943dbc6d4702c0f0c0ca196
zdudusz <eduardogamese8@gmail.com>
2026-09-04 19:03:30 -0300

    Adicionando migrations

---
+++ ScientificCalculatorApi.Infraestructure/Migrations/20260904215300_AddCalculationHistory.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScientificCalculatorApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCalculationHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_CalculationHistory_UserId",
+                table: "CalculationHistory",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CalculationHistory_Users_UserId",
+                table: "CalculationHistory",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CalculationHistory_Users_UserId",
+                table: "CalculationHistory");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CalculationHistory_UserId",
+                table: "CalculationHistory");
+        }
+    }
+}
