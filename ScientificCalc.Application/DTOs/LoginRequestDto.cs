d4bf3d3e10ac495b56c1743ce98c378e1b000ee9
zdudusz <eduardogamese8@gmail.com>
2026-09-04 14:21:18 -0300

    feat: adicionar controller de autenticação e endpoint de login com geração de token JWT

---
+++ ScientificCalc.Application/DTOs/LoginRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScientificCalcApi.Application.DTOs
+{
+    public class LoginRequestDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
--- ScientificCalcAPI/Controllers/AuthController.cs
+++ ScientificCalcAPI/Controllers/AuthController.cs
@@ -1,16 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ScientificCalcApi.Application.Applications;
+using ScientificCalcApi.Application.DTOs;
 using ScientificCalcApi.Application.Services;
-
+using ScientificCalcAPI.Core.Models.InputModels;
 namespace ScientificCalcAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class AuthController : MainController
     {
+        public AuthController(LoginApplication loginApplication, TokenService tokenService)
+        {
+            _loginApplication = loginApplication;
+            _tokenService = tokenService;
+        }
         private readonly LoginApplication _loginApplication;
         private readonly TokenService _tokenService;
 
-        //implementando ainda
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto _loginRequestDto)
+        {
+           var user = await _loginApplication.LoginAsync(_loginRequestDto.Email,_loginRequestDto.Password);
+            var token =  _tokenService.GenerateToken(user);
+            return Ok(token);
+        }
     }
 }
