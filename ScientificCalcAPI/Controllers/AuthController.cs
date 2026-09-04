59e4ce8a6bdfd9acaf409ea99a56eda6d3671899
zdudusz <eduardogamese8@gmail.com>
2026-09-04 14:08:44 -0300

    Adicionando classe AuthController

--- ScientificCalcAPI/Controllers/AuthController.cs
+++ ScientificCalcAPI/Controllers/AuthController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ScientificCalcApi.Application.Applications;
+using ScientificCalcApi.Application.Services;
 
 namespace ScientificCalcAPI.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class AuthController : MainController
     {
+        private readonly LoginApplication _loginApplication;
+        private readonly TokenService _tokenService;
 
+        //implementando ainda
     }
 }
